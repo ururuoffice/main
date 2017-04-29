1f47136d6c3f599daed4f808d3f66791cc765941
Oleg Zaporozhchenko <olegz@jetruby.com>
2017-04-29 16:40:13 +0300

    Implement Office state handler

--- web/controllers/office_controller.ex
+++ web/controllers/office_controller.ex
@@ -9,57 +9,50 @@ defmodule Main.OfficeController do
     office = %{
       id: "1",
       title: "JetrubyOffice",
-      rooms: [
-        %{
-          id: "1",
-          title: "",
-          dimentions: %{ width: "220px", height: "220px", top: "220px", left: "500px" },
-          places: [
-            %{
-              id: "1",
-              title: "",
-              coords: %{ top: "10px", left: "10px" },
-              user: nil
-            },
-            %{
-              id: "2",
-              title: "",
-              coords: %{ top: "10px", left: "70px" },
-              user: nil
-            },
-            %{
-              id: "3",
-              title: "",
-              coords: %{ top: "10px", left: "130px" },
-              user: nil
-            },
-          ],
-        },
-        %{
-          title: "",
+      rooms: %{
+        "1" =>   %{
+            title: "Room 1",
+            dimentions: %{ width: "220px", height: "220px", top: "220px", left: "500px" },
+            places: %{
+              "1" => %{
+                title: "",
+                coords: %{ top: "10px", left: "10px" },
+                user: nil
+              },
+              "2" => %{
+                title: "",
+                coords: %{ top: "10px", left: "70px" },
+                user: nil
+              },
+              "3" => %{
+                title: "",
+                coords: %{ top: "10px", left: "130px" },
+                user: nil
+              }
+            }
+          },
+        "2" => %{
+          title: "Room 2",
           dimentions: %{ width: "220px", height: "220px", top: "220px", left: "1000px" },
-          places: [
-            %{
-              id: "1",
+          places: %{
+            "1" => %{
               title: "",
               coords: %{ top: "10px", left: "10px" },
               user: nil
             },
-            %{
-              id: "2",
+            "2" => %{
               title: "",
               coords: %{ top: "10px", left: "70px" },
               user: nil
             },
-            %{
-              id: "3",
+            "3" => %{
               title: "",
               coords: %{ top: "10px", left: "130px" },
               user: nil
-            },
-          ],
+            }
+          }
         }
-      ]
+      }
     }
 
     render conn, "show.json", office: office
