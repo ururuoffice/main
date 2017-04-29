dc761ac4dee422acad725e265b19df674cfb8e8d
Bogdan <d@MacBook-Pro-Bogdan.local>
2017-04-29 12:45:05 +0300

    Setup basic cimponent structure

--- web/static/components/App.vue
+++ web/static/components/App.vue
@@ -1,22 +1,53 @@
 <template>
-  <div class="my-app">
-    <h1>Vuechat</h1>
+  <div id="app">
+    <img src="images/office_plan.jpg">
+    <room v-for="room in rooms" v-bind:dimentions="room.dimentions" v-bind:places="room.places"></room>
   </div>
 </template>
 
 <script>
+import Room from './Room';
 
 export default {
-}
+  name: 'app',
+  components: {
+    Room,
+  },
+  data() {
+    return {
+      rooms: [
+        {
+          title: '',
+          dimentions: { width: '220px', height: '220px', top: '220px', left: '500px' },
+          places: [
+            {
+              title: '',
+              coords: { top: '10px', left: '10px' },
+            },
+            {
+              title: '',
+              coords: { top: '10px', left: '70px' },
+            },
+          ],
+        },
+        {
+          title: '',
+          dimentions: { width: '220px', height: '220px', top: '220px', left: '1000px' },
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style lang="sass">
-.my-app {
-  margin-left: auto;
-  margin-right: auto;
-  width: 800px;
-  h1 {
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
     text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    position: relative;
   }
-}
 </style>
