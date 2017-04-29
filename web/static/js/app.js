caed99ae8b32446e6609a0d7d8e669607cf0b4a3
Bogdan <d@MacBook-Pro-Bogdan.local>
2017-04-29 12:08:24 +0300

    Setup vue on project

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,9 +19,20 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 import socket from "./socket"
+import Vue from 'vue'
+import App from "../components/App.vue"
+
+Vue.component('app', App)
 
 // Now that you are connected, you can join channels with a topic:
 window.channel = socket.channel("room:lobby", {})
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
+
+new Vue({
+  el: '#app',
+  render(createElement) {
+    return createElement(App, {})
+  }
+});
